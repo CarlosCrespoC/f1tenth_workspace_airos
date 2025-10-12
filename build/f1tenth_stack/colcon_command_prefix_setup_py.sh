e996b24475528b6b49a68d4e51e2b7c59a4bb633
CarlosCrespoC <caedcres@espol.edu.ec>
2025-10-12 00:10:14 -0500

    car

---
+++ build/f1tenth_stack/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,8 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/jetson/f1tenth2_ws/src/f1tenth_workspace_airos/install/ackermann_mux/share/ackermann_mux/package.sh"
+. "/home/jetson/f1tenth2_ws/src/f1tenth_workspace_airos/install/teleop_tools_msgs/share/teleop_tools_msgs/package.sh"
+. "/home/jetson/f1tenth2_ws/src/f1tenth_workspace_airos/install/vesc_msgs/share/vesc_msgs/package.sh"
+. "/home/jetson/f1tenth2_ws/src/f1tenth_workspace_airos/install/joy_teleop/share/joy_teleop/package.sh"
+. "/home/jetson/f1tenth2_ws/src/f1tenth_workspace_airos/install/vesc_ackermann/share/vesc_ackermann/package.sh"
+. "/home/jetson/f1tenth2_ws/src/f1tenth_workspace_airos/install/vesc_driver/share/vesc_driver/package.sh"
+. "/home/jetson/f1tenth2_ws/src/f1tenth_workspace_airos/install/vesc/share/vesc/package.sh"
