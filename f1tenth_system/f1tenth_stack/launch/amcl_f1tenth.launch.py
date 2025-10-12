7016b43d5cc77b4ba4f9f1f5c283209bd358ead7
CarlosCrespoC <caedcres@espol.edu.ec>
2025-10-12 13:48:16 -0500

    ErrorAMCL

--- f1tenth_system/f1tenth_stack/launch/amcl_f1tenth.launch.py
+++ f1tenth_system/f1tenth_stack/launch/amcl_f1tenth.launch.py
@@ -7,12 +7,13 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    pkg_share = get_package_share_directory('f1tenth_stack') # <--- CAMBIA ESTO
+    # Asegúrate de que 'f1tenth_stack' es el nombre correcto de tu paquete
+    pkg_share = get_package_share_directory('f1tenth_stack') 
     
     # Declarar el argumento para la ruta del mapa
     map_file_arg = DeclareLaunchArgument(
         'map',
-        default_value=os.path.join(pkg_share, 'maps', 'mapa_lab.yaml'), # <--- RUTA A TU MAPA
+        default_value=os.path.join(pkg_share, 'maps', 'mapa_lab.yaml'), # <-- RUTA A TU MAPA
         description='Full path to map file to load'
     )
 
@@ -30,22 +31,21 @@ def generate_launch_description():
         executable='amcl',
         name='amcl',
         output='screen',
-        parameters=[{'use_sim_time': False, # <--- Cambia a True si usas simulación
+        parameters=[{'use_sim_time': False,        # <-- Cambia a True si usas simulación
                      'alpha1': 0.1,
                      'alpha2': 0.1,
-		     'alpha3': 0.1,
-		     'alpha4': 0.1,
-                     'base_frame_id': 'base_link', # <--- Frame base de tu robot
-                     'odom_frame_id': 'odom',      # <--- Frame de odometría
-                     'global_frame_id': 'map'
-		     'max_particles': 2000,
-		     'min_particles': 500,
-		     'update_min_d': 0.05,
-		     'update_min_a': 0.15,
-		     'laser_max_beams':360,
-			}]    # <--- Frame del mapa
-    		     
-	)
+                     'alpha3': 0.1,
+                     'alpha4': 0.1,
+                     'base_frame_id': 'base_link',   # <-- Frame base de tu robot
+                     'odom_frame_id': 'odom',        # <-- Frame de odometría
+                     'global_frame_id': 'map',       # <-- ¡COMA AÑADIDA AQUÍ!
+                     'max_particles': 2000,
+                     'min_particles': 500,
+                     'update_min_d': 0.05,
+                     'update_min_a': 0.15,
+                     'laser_max_beams': 360
+                    }]
+    )
 
     # Lanzar el gestor de ciclo de vida para activar los nodos
     lifecycle_manager_node = Node(
