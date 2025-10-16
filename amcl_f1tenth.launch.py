4ef54a493ed0ce1dfc9d7ecbd725a4546d124760
CarlosCrespoC <116905198+CarlosCrespoC@users.noreply.github.com>
2025-10-16 16:24:50 -0500

    ARCHIVOS PARA VERIFICAR

---
+++ amcl_f1tenth.launch.py
@@ -0,0 +1,65 @@
+# amcl_launch.py
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    # Asegúrate de que 'f1tenth_stack' es el nombre correcto de tu paquete
+    pkg_share = get_package_share_directory('f1tenth_stack') 
+    
+    # Declarar el argumento para la ruta del mapa
+    map_file_arg = DeclareLaunchArgument(
+        'map',
+        default_value=os.path.join(pkg_share, 'maps', 'CATO3.yaml'), # <-- RUTA A TU MAPA
+        description='Full path to map file to load'
+    )
+
+    # Lanzar el servidor de mapas
+    map_server_node = Node(
+        package='nav2_map_server',
+        executable='map_server',
+        name='map_server',
+        parameters=[{'yaml_filename': LaunchConfiguration('map')}]
+    )
+
+    # Lanzar AMCL
+    amcl_node = Node(
+        package='nav2_amcl',
+        executable='amcl',
+        name='amcl',
+        output='screen',
+        parameters=[{'use_sim_time': False,        # <-- Cambia a True si usas simulación
+                     'alpha1': 0.2,
+                     'alpha2': 0.2,
+                     'alpha3': 0.2,
+                     'alpha4': 0.2,
+                     'base_frame_id': 'base_link',   # <-- Frame base de tu robot
+                     'odom_frame_id': 'odom',        # <-- Frame de odometría
+                     'global_frame_id': 'map',       
+                     'max_particles': 2000,
+                     'min_particles': 500,
+                     'update_min_d': 0.01,
+                     'update_min_a': 0.01,
+                     'laser_max_beams': 500
+                    }]
+    )
+
+    # Lanzar el gestor de ciclo de vida para activar los nodos
+    lifecycle_manager_node = Node(
+        package='nav2_lifecycle_manager',
+        executable='lifecycle_manager',
+        name='lifecycle_manager_localization',
+        output='screen',
+        parameters=[{'autostart': True},
+                    {'node_names': ['map_server', 'amcl']}]
+    )
+
+    return LaunchDescription([
+        map_file_arg,
+        map_server_node,
+        amcl_node,
+        lifecycle_manager_node
+    ])
