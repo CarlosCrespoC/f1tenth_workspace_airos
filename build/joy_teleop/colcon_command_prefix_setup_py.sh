7a44408e9515c481594b45e39f3e6a49b512e3f5
CarlosCrespoC <caedcres@espol.edu.ec>
2025-10-16 12:50:48 -0500

    Vuelve porfavor

--- build/joy_teleop/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/jetson/f1tenth2_ws/src/f1tenth_workspace_airos/install/teleop_tools_msgs/share/teleop_tools_msgs/package.sh"
